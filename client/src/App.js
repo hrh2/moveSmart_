90ae4eed258c108ecf48a4186250e0f28cd743ce
Hirwa <gakundohope5@gmail.com>
2023-05-09 08:58:06 +0200

    updated homepage routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Route, Routes, Navigate } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from './components/Login';
 import Signup from './components/Signup';
+import BookForm from './components/BookingForm'
 import Home from './components/Home';
-
-
+import ContactUs from './components/ContactUs';
 
 function App() {
   const user = localStorage.getItem("token");
   return (
     <Routes>
-      {user && <Route path="/" exact element={<Home/>} />}
+      <Route path="/" element={<Home />}>
+        <Route index element={<BookForm />} />
+        <Route path="/ContactUs" element={<ContactUs/>} />
+      </Route>
       <Route path="/signup" exact element={<Signup />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/" element={<Navigate replace to="/login" />} />
@@ -18,4 +25,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
