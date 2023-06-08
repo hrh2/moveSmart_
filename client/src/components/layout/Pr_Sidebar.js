0fe82679bdc27cd2ffd3a7c46bf494fcd2e627d8
Hirwa Rukundo Hope <gakundohope5@gmail.com>
2023-06-08 15:53:55 +0200

    private boooking sidebar component

--- client/src/components/layout/Pr_Sidebar.js
+++ client/src/components/layout/Pr_Sidebar.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import { Link } from 'react-router-dom';
+import logo1 from '../img/logo.png'
+import {FaHome} from 'react-icons/fa'
+import{RiDashboardFill} from 'react-icons/ri'
+import {FiLogOut} from 'react-icons/fi'
 
-export default function Pr_Sidebar() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function SideBar() {
+     return (
+          <div className="sidebarPr container-fluid d-flex flex-column justify-content-center align-items-center vh-100">
+               <img src={logo1} alt="logo" className="mx-auto Logo" style={{ position: 'absolute', top: '.1em' }} />
+               <Link to="/userProfile" className="btn btn-lg text-white fw-bold active py-3 border-0">
+                    <FaHome size="2em" className=' hover:text-sky-900 '/>
+               </Link>
+               <Link to="/userProfile" className="btn btn-lg text-white fw-bold py-3 border-0">
+                    <RiDashboardFill size="2em" className='hover:text-sky-900 '/>
+               </Link>
+               <Link to="/userProfile" className="btn btn-lg text-warning fw-bold py-3 border-0" style={{position:'absolute',bottom:".1em"}}>
+                <p>logout</p>
+                    <FiLogOut size="2em" className='hover:text-sky-900 '/>
+               </Link>
+          </div>
+     );
 }
